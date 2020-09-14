7f558ad3692616552bbe937cd066c5fe2900fb8b
asiabachanowicz <asiabachanowicz@gmail.com>
2020-09-14 20:35:13 +0200

    css update

--- server/src/main/java/com/ibm/server/FrontendController.java
+++ server/src/main/java/com/ibm/server/FrontendController.java
@@ -8,13 +8,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class FrontendController {
 
     @GetMapping("/")
-    public String index() {
+    public String main() {
 
-        return "index";
+        return "main";
     }
-    @GetMapping("/login.html")
+    @GetMapping("/login_register.html")
     public String login() {
 
         return "login";
+    }
+    @GetMapping("/patients.html")
+    public String patients() {
+
+        return "patients";
+    }
+    @GetMapping("/single_patient.html")
+    public String single_patient() {
+
+        return "single_patient";
+    }
+    @GetMapping("/exercises.html")
+    public String exercises() {
+
+        return "exercises";
     }
 }
