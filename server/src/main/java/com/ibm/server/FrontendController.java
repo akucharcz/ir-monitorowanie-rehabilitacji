aeb5b19b4bc481b654af43ea11d16b41d61bf08f
ZmAleksandra <olazmijewska28@wp.pl>
2020-09-15 22:05:40 +0200

    html charts - datas not from database

--- server/src/main/java/com/ibm/server/FrontendController.java
+++ server/src/main/java/com/ibm/server/FrontendController.java
@@ -1,8 +1,12 @@
 package com.ibm.server;
 
-
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
 
 @Controller
 public class FrontendController {
@@ -23,8 +27,13 @@ public class FrontendController {
         return "patients";
     }
     @GetMapping("/single_patient.html")
-    public String single_patient() {
-
+    public String single_patient(Model model) {
+    Map<String, Integer> surveyMap = new LinkedHashMap<>();
+    surveyMap.put("Godzina",40 );
+        surveyMap.put("Doba",20 );
+        surveyMap.put("Tydzień",10 );
+        surveyMap.put("Miesiąc", 5);
+        model.addAttribute("surveyMap", surveyMap);
         return "single_patient";
     }
     @GetMapping("/exercises.html")
